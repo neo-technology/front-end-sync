b4c305c603683045d6753d06d50c296d0f8b58ed
Satia Herfert <satiaherfert@gmx.de>
2018-11-29 12:02:32 +0100

    Restore history for poms/NOTICE/LICENSE etc.
    These files were removed or touched while moving the frontend into
    Neo4j, but we don't want these changes to be in openCypher frontend.

--- publish-opencypher-frontend.sh
+++ publish-opencypher-frontend.sh
@@ -10,12 +10,10 @@
 #	commit6						commit 414
 #	commit5						commit 413
 
-# TODO test 3.5 forward merge with conflict
-# TODO test 3.4 forward merge
-
 # CONFIGURATION
 OPENCYPHER_REPO=sherfert/front-end.git
-OPENCYPHER_COMMIT=01148edb5ca9b49c3173a6f8d7c79c877ddb2f74
+# This is the last commit in the frontend. Export since we need it in another script as well
+export OPENCYPHER_COMMIT=01148edb5ca9b49c3173a6f8d7c79c877ddb2f74
 OPENCYPHER_BRANCH_NAME=9.0-sync-test2
 
 NEO4J_REPO=sherfert/neo4j.git
@@ -27,6 +25,8 @@ NEO4J_BRANCH_NAME=4.0-sync-test
 # INTERNALS
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 CONVERT_PACKAGES_BIN="$DIR/convert-packages.sh"
+RESTORE_FILES_BIN="$DIR/restore-pom-license-notices.sh"
+
 NEO4J_BRANCH=origin/$NEO4J_BRANCH_NAME
 
 # fetch source trees
@@ -41,15 +41,13 @@ NEO4J_CUTOFF=`git log --format='%H' "$NEO4J_COMMIT^" | head -n 1`
 # remove everything apart from the front-end subdirectory
 git filter-branch --force --prune-empty --subdirectory-filter public/community/cypher/front-end -- $NEO4J_BRANCH --not $NEO4J_CUTOFF
 
-# remove poms
-#git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch pom.xml */pom.xml' -- $NEO4J_BRANCH --not $NEO4J_CUTOFF
-# TODO this does not work with replace
-# TODO instead we should apply diffs inside of convert.packages.sh
-# TODO and we should also ignore LICENSE LICENSES and NOTICE and all that stuff
-
 # change directories and packages in all commits to org.opencypher.v9_0
 git filter-branch --force --prune-empty --tree-filter "$CONVERT_PACKAGES_BIN" -- $NEO4J_BRANCH --not $NEO4J_CUTOFF
 
+# change all commits, such that they keep poms/notice/license files untouched
+git filter-branch --force --prune-empty --tree-filter "$RESTORE_FILES_BIN" -- $NEO4J_BRANCH --not $NEO4J_CUTOFF
+
+# put the history on top of the opencypher history
 git replace $NEO4J_CUTOFF $OPENCYPHER_COMMIT
 git filter-branch --force $OPENCYPHER_COMMIT..$NEO4J_BRANCH
 
@@ -59,12 +57,5 @@ rm -rf .git/refs/original
 # switch to the branch (will be detached HEAD)
 git checkout $NEO4J_BRANCH
 
-# Ensure things compile and tests are green
-mvn clean test
-
-# push it
-git push pub HEAD:$OPENCYPHER_BRANCH_NAME
-
-# Post-cleanup (only necessary for local runs)
-cd ..
-rm -rf neo4j
+# Ensure things compile and tests are green, if so, push. Afterwards clean up
+mvn clean test && git push pub HEAD:$OPENCYPHER_BRANCH_NAME && cd .. && rm -rf neo4j
---
+++ restore-pom-license-notices.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for path in .gitignore .travis.yml NOTICE.txt **/NOTICE.txt LICENSES.txt **/LICENSES.txt pom.xml */pom.xml build ; do
+    git checkout -q $OPENCYPHER_COMMIT -- $path 2>/dev/null
+done
+exit 0 # in case the last "git checkout" failed, override its status

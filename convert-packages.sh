94a9818bf755de5328c5b37105d4a3dc06fcef9d
Louise Söderström <louise.matematik@gmail.com>
2021-02-15 16:26:06 +0100

    Fix sync of .jj file

--- convert-packages.sh
+++ convert-packages.sh
@@ -34,6 +34,23 @@ for d in ast ast-factory cypher-macros expressions frontend javacc-parser neo4j-
                         sed -n '/package /,$p' "$sourceFile.temp" >> "$sourceFile"
                         rm "$sourceFile.temp"
                     done
+
+                    # Special handling for cypher.jj file
+                    for jjFile in `find "$sourceDir/$OPENCYPHER_DIR" -type f -name "cypher.jj"`; do
+                       # change to open cypher package
+                        cat "$jjFile" | sed "s/$neo4jPackage/$OPENCYPHER_PACKAGE/g" > "$jjFile.temp"
+
+                        # copy in all code up until the search string (i.e. before the Neo4j license)
+                        searchString="PARSER_BEGIN(Cypher)"
+                        sed -n "1,/${searchString}/p" "$jjFile.temp" > "$jjFile"
+
+                        # insert the open cypher license
+                        cat $OPENCYPHER_LICENSE >> "$jjFile"
+
+                        # copy in the rest of the code
+                        sed -n '/package /,$p' "$jjFile.temp" >> "$jjFile"
+                        rm "$jjFile.temp"
+                    done
                 fi
 			fi
 		done
